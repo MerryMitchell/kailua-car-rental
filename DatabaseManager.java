52abd0e505bcea5c4d416359075538945f459d2c
Merry <Merry.j.mitchell2@gmail.com>
2025-02-21 09:39:01 +0100

    Update DatabaseManager.java

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseManager {
     private static final String URL = "jdbc:mysql://localhost:3306/KailuaCarRental";
     private static final String USER = "root";
-    private static final String PASSWORD = "Orangetomato12";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
